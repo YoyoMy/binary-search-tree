4b80e75fdb624383868d4155cebb483b6f4b9bdf
YoyoMy <35624284+YoyoMy@users.noreply.github.com>
2022-04-22 07:48:54 +0200

    2 clases that i forgot to save before initial push

--- src/main/java/com/example/BinaryTree.java
+++ src/main/java/com/example/BinaryTree.java
@@ -1,6 +1,48 @@
 package com.example;
 
-public class BinaryTree {
+import java.util.ArrayList;
+
+public class BinaryTree<T> {
     BinaryTreeNode root;
     int size;
+
+    public BinaryTreeNode getRoot() {
+        return root;
+    }
+    public void setRoot(BinaryTreeNode root) {
+        this.root = root;
+    }
+    public boolean isEmpty()
+    {
+        return size==0;
+    }
+
+    public int size()
+    {
+        return size;
+    }
+    public boolean contains(T element)
+    {
+        return false;
+    }
+    public ArrayList<T> inOrder()
+    {
+        return null;
+    }
+    public ArrayList<T> preOrder()
+    {
+        return null;
+    }
+    public ArrayList<T> postOrder()
+    {
+        return null;
+    }
+    public ArrayList<T> levelOrder()
+    {
+        return null;
+    }
+    public int height()
+    {
+        return 0;
+    }
 }
