0118aa65426bec9f504ec1ccd35b5c940610190e
NoHop3 <u_sux@abv.bg>
2022-04-22 11:13:33 +0200

    Adding the TreePrint class from the provided resources. Implementing contains + in order methods.

--- src/main/java/com/example/BinaryTree.java
+++ src/main/java/com/example/BinaryTree.java
@@ -6,6 +6,10 @@ public class BinaryTree<T> {
     BinaryTreeNode root;
     int size;
 
+    public BinaryTree(){
+        this.root = null;
+    }
+
     public BinaryTreeNode getRoot() {
         return root;
     }
@@ -21,14 +25,55 @@ public class BinaryTree<T> {
     {
         return size;
     }
-    public boolean contains(T element)
-    {
-        return false;
+    public boolean contains(int element) {
+        return containsNodeRecursive(root, element);
     }
-    public ArrayList<T> inOrder()
-    {
-        return null;
+
+    /** Contains */
+
+    private boolean containsNodeRecursive(BinaryTreeNode current, int value) {
+        if (current == null) {
+            return false;
+        }
+        if (value == (int)current.getElement()) {
+            return true;
+        }
+        return value < (int)current.getElement()
+            ? containsNodeRecursive(current.getLeftChild(), value)
+            : containsNodeRecursive(current.getRightChild(), value);
     }
+
+    /** In order */
+
+    public ArrayList<Integer> inOrder() {
+        ArrayList<Integer> result = new ArrayList();
+        orderInorder(root, result);
+        return result;
+    }
+
+    void orderInorder(BinaryTreeNode node, ArrayList<Integer> result) {
+        if (node == null)
+            return;
+        orderInorder(node.getLeftChild(), result);
+        result.add((int)node.getElement());
+        orderInorder(node.getRightChild(), result);
+    }
+
+    public ArrayList<BinaryTreeNode> inOrderNode() {
+        ArrayList<BinaryTreeNode> result = new ArrayList();
+        orderInorderNode(root, result);
+        return result;
+    }
+    void orderInorderNode(BinaryTreeNode node, ArrayList<BinaryTreeNode> result) {
+        if (node == null)
+            return;
+        orderInorderNode(node.getLeftChild(), result);
+        result.add(node);
+        orderInorderNode(node.getRightChild(), result);
+    }
+
+    /** Preorder */
+
     public ArrayList<T> preOrder()
     {
         return null;
